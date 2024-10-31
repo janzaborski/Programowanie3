46a381d15372f5f0b1cfa68a3394916ce3808ef3
janzaborski <janzaborski8@gmail.com>
2024-10-31 08:37:01 +0100

    sekwencje

---
+++ Lab8_2324/SequenceOperations.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab08
+{
+	interface IMultiSeqOperation
+	{
+		// TODO: Add your code here.
+        
+		string Info { get; }
+	}
+
+	static class SequenceHelpers
+	{
+		// TODO: Add your code here.
+	}
+
+    // TODO: Add your code here.
+}
---
+++ Lab8_2324/Sequences.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static Lab08.SequenceHelpers;
+
+namespace Lab08
+{
+	interface ISequence
+	{
+		string Name { get; }
+	}
+
+    // TODO: Add your code here
+}
